0d253f82171b35122ee095466e848e71885771c5
gitRoee <roeekl0@gmail.com>
2022-12-14 21:54:06 +0200

    cleaned a bit of the code except for mjs files

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,10 +1,9 @@
-// src/pages/api/examples.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../server/db/client';
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-	const messages = await prisma.archivedMessage.findMany({});
-	res.status(200).json(messages);
+    const messages = await prisma.archivedMessage.findMany({});
+    res.status(200).json(messages);
 };
 
 export default examples;
--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,19 +1,17 @@
-// src/server/db/client.ts
-import { PrismaClient } from "@prisma/client";
-import { env } from "../../env/server.mjs";
+import { PrismaClient } from '@prisma/client';
+import { env } from '../../env/server.mjs';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log:
+            env.NODE_ENV === 'development'
+                ? ['query', 'error', 'warn']
+                : ['error'],
+    });
 
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
 }
